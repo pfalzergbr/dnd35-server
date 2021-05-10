99196505b96610dc8b9b1d43954a9f2bddc41672
Gabor Pfalzer <pfalzer@gmail.com>
2021-05-10 11:27:15 +0100

    connect mongoose, express and apollo server in index.ts

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { ApolloServer } from 'apollo-server-express';
+import Express from 'express';
+import 'reflect-metadata';
+import { buildSchema } from 'type-graphql';
+import { connect } from 'mongoose';
+
+import keys from './config/keys';
+
+//Resolvers
+import { UserResolver } from './resolvers/User'
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [ UserResolver],
+    emitSchemaFile: true,
+    validate: false,
+  });
+
+  const mongoose = await connect(keys.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connection
+
+
+  const server = new ApolloServer({ schema });
+  const app = Express();
+  server.applyMiddleware({ app });
+  app.listen({ port: keys.PORT }, () => {
+    console.log(
+      `🐉 Server ready for adventure at http://localhost:${keys.PORT}${server.graphqlPath} 🧙`
+    );
+  });
+};
+
+
+main().catch(error => console.log(error, 'error'))
+
