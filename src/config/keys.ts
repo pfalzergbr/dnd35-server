1219824336c28cf8325655144a671913e2347009
Gabor Pfalzer <pfalzer@gmail.com>
2021-05-10 07:03:42 +0100

    initial commit

---
+++ src/config/keys.ts
@@ -0,0 +1,17 @@
+export interface Keys {
+  COOKIE_KEY: string;
+  PORT: number
+  MONGO_URI: string
+}
+
+let keys: Keys;
+
+if (process.env.NODE_ENV === 'production') {
+  keys = require('./prodenv');
+} else if (process.env.NODE_ENV === 'test') {
+  keys = require('./testenv');
+} else {
+  keys = require('./devenv');
+}
+
+export default keys;
---
+++ src/config/prodenv.ts
@@ -0,0 +1,5 @@
+module.exports = {
+  COOKIE_KEY: process.env.COOKIE_KEY,
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI
+};
