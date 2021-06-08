c07fec1af06e6dc31362fe2227884c8494adcfb6
Gabor Pfalzer <pfalzer@gmail.com>
2021-06-08 09:10:02 +0100

    extract CharacterLink from User model

---
+++ src/entities/users/CharacterLink.ts
@@ -0,0 +1,29 @@
+import { ObjectType, Field, ID } from 'type-graphql';
+import { prop, mongoose } from '@typegoose/typegoose';
+
+@ObjectType({ description: 'Short character reference on user object' })
+export class CharacterLink {
+  @Field(() => ID)
+  @prop({ required: true })
+  characterId!: mongoose.Types.ObjectId;
+
+  @Field()
+  @prop({ required: true })
+  name!: String;
+
+  @Field(() => String, {nullable: true})
+  @prop()
+  race!: String | null;
+
+  @Field(() => String, {nullable: true})
+  @prop()
+  class!: String | null; 
+
+  @Field()
+  @prop({required: true, default: 1})
+  level!: Number;
+
+  @Field()
+  @prop({required: true, default: false})
+  isCompleted!: Boolean;
+}
--- src/entities/users/User.ts
+++ src/entities/users/User.ts
@@ -1,33 +1,7 @@
 import { ObjectType, Field, ID } from 'type-graphql';
 import { prop, getModelForClass, mongoose } from '@typegoose/typegoose';
 import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
-
-@ObjectType({ description: 'Short character reference on user object' })
-export class CharacterLink {
-  @Field(() => ID)
-  @prop({ required: true })
-  characterId!: mongoose.Types.ObjectId;
-
-  @Field()
-  @prop({ required: true })
-  name!: String;
-
-  @Field(() => String, {nullable: true})
-  @prop()
-  race!: String | null;
-
-  @Field(() => String, {nullable: true})
-  @prop()
-  class!: String | null; 
-
-  @Field()
-  @prop({required: true, default: 1})
-  level!: Number;
-
-  @Field()
-  @prop({required: true, default: false})
-  isCompleted!: Boolean;
-}
+import { CharacterLink } from './CharacterLink'
 
 @ObjectType({ description: 'User model' })
 export class User extends TimeStamps {
