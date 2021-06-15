7d1994328b9d5a948f51b4447037f6047c231b9c
Gabor Pfalzer <pfalzer@gmail.com>
2021-06-15 04:38:18 +0100

    finish physical stats entities

--- src/entities/characters/PhysicalStats.ts
+++ src/entities/characters/PhysicalStats.ts
@@ -33,8 +33,64 @@ export class Health {
 }
 
 @ObjectType()
+export class Initiative {
+   @Field({description: 'Total Initiative of the Character'})
+   @prop()
+   totalInitiative!: Number
+
+   @Field({description: 'Base Initiative of the Character'})
+   @prop()
+   baseInitiative!: Number
+
+   @Field(() => [StatModifier], {description: 'Array of initiative modifiers'})
+   @prop()
+   initiativeModifiers!: StatModifier[]
+}
+
+@ObjectType()
+export class Speed {
+  @Field({description: 'Total land speed of the character'})
+  @prop()
+  totalSpeed!: Number
+   
+  @Field({description: 'Base land speed of the character'})
+  @prop()
+  baseSpeed!: Number
+  
+  @Field(() => [StatModifier], {description: 'Array of speed modifiers'})
+  @prop()
+  speedModifiers!: StatModifier[]
+}
+
+@ObjectType()
+export class ArmorClass {
+  @Field({description: 'Total Armor Class'})
+  @prop()
+  totalAC!: Number; 
+
+  @Field({description: 'Base Armor Class'})
+  @prop()
+  baseAC!: Number; 
+
+  @Field(() => [StatModifier], {description: 'Array of Armor Class Modifiers'})
+  @prop()
+  ACModifiers!: StatModifier[]
+}
+
+
+@ObjectType()
 export class PhysicalStats {
   @Field(() => Health, { description: 'Current and maximum health of the Character'})
   @prop()
   health!: Health
+
+  @Field(() => Initiative, {description: 'Characters Initiative value'})
+  @prop()
+  initiative!: Initiative
+
+  @Field(() => Speed, {description: 'Characters Initiative value'})
+  @prop()
+  speed!: Speed
 }
+
+
